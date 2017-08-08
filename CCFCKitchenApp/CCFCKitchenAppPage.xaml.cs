ba561f508928cbccdd853e970b5374e9a541b61f
Prosenjit Goswami <KolkataMobility@Mac-mini-11.local>
2017-08-08 18:41:24 +0530

    New Kitchen App
    Adding new Project

---
+++ CCFCKitchenApp/CCFCKitchenAppPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CCFCKitchenApp
+{
+	public partial class CCFCKitchenAppPage : ContentPage
+	{
+		public CCFCKitchenAppPage()
+		{
+			InitializeComponent();
+			Title = "Home";
+			var tapGestureRecognizer = new TapGestureRecognizer();
+
+			tapGestureRecognizer.Tapped += (s, e) => {
+				OnTapGestureRecognizerTapped(s,e);
+			};
+
+			drinksItemImg.GestureRecognizers.Add(tapGestureRecognizer);
+			foodItemImg.GestureRecognizers.Add(tapGestureRecognizer);
+			dessertsItemImg.GestureRecognizers.Add(tapGestureRecognizer);
+			beveragesItemImg.GestureRecognizers.Add(tapGestureRecognizer);
+
+		}
+
+		void OnTapGestureRecognizerTapped(object sender, EventArgs args){
+			var imageSender = (Image)sender;
+			Navigation.PushAsync(new CCFCKitchenAppFoodPage());
+		}
+	}
+}
---
+++ CCFCKitchenApp/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CCFCKitchenApp
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+			var tapGestureRecognizerLogin = new TapGestureRecognizer();
+			tapGestureRecognizerLogin.NumberOfTapsRequired = 1;
+			tapGestureRecognizerLogin.Tapped += (s, e) => {
+				OnLoginButtonClicked();
+			};
+			LoginInLbl.GestureRecognizers.Add(tapGestureRecognizerLogin);	
+
+		}
+		void OnLoginButtonClicked()
+		{
+			Navigation.PushAsync(new CCFCKitchenAppPage());
+		}
+	}
+}
